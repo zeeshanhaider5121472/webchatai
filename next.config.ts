39c3d8d711f446130cf369d16714b4e307af3b6d
zeeshan Haider <shani721287@gmail.com>
2026-04-16 19:47:19 +0500

    added chromimium min for vercel support v1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@sparticuz/chromium-min", "puppeteer-core"],
+
+  // Extend webpack config
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Treat puppeteer as external so it won't be bundled
+      config.externals.push({
+        puppeteer: "puppeteer",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
