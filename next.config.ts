4587477504ac8b662b7a6c6c42150b59ef5c9892
zeeshan Haider <shani721287@gmail.com>
2026-04-16 13:59:32 +0500

    few tweaks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  serverExternalPackages: ["@sparticuz/chromium-min", "puppeteer-core"],
 };
 
 export default nextConfig;
